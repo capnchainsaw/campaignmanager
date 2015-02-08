f0bdad3dc7b665f788507082bd6b8d0ae3c4eabc
James Johnson <tiasdungeon@gmail.com>
2015-02-08 00:10:24 -0800

    fixed some minor issues

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
 <title>Placeholder index</title>
 </header>
 <body>
-This page is a placeholder.
-<a href="view.php?id=1,type=campaign">Test view campaign with ID 1</a><br/>
-<a href="view.php?id=1,type=user">Test view user with ID 1</a><br/>
-<a href="view.php?id=1,type=character">Test view character with ID 1</a><br/>
+This page is a placeholder.<br/>
 <br/>
-<a href="edit.php?id=1,type=campaign">Test edit campaign with ID 1</a><br/>
-<a href="edit.php?id=1,type=user">Test edit user with ID 1</a><br/>
-<a href="edit.php?id=1,type=character">Test edit character with ID 1</a><br/>
+<a href="view.php?id=1&type=campaign">Test view campaign with ID 1</a><br/>
+<a href="view.php?id=1&type=user">Test view user with ID 1</a><br/>
+<a href="view.php?id=1&type=character">Test view character with ID 1</a><br/>
+<br/>
+<a href="edit.php?id=1&type=campaign">Test edit campaign with ID 1</a><br/>
+<a href="edit.php?id=1&type=user">Test edit user with ID 1</a><br/>
+<a href="edit.php?id=1&type=character">Test edit character with ID 1</a><br/>
 </body>
 </html>
