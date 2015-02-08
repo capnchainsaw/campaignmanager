ec6545e63bed7051c3046f2d9918d33bde5924f0
James Johnson <tiasdungeon@gmail.com>
2015-02-07 22:56:44 -0800

    adding placeholder index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Dungeons and Dragons Campaign Manager 
+ * by James Johnson (tiasdungeon@gmail.com) */
+
+?>
+<html>
+<header>
+<title>Placeholder index</title>
+</header>
+<body>
+This page is a placeholder.
+</body>
+</html>
